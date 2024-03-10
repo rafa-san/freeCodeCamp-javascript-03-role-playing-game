29097f257fa83263ad9dfe121828781ab92bf4b2
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-03-10 11:48:01 -0300

    Criei novas funções e objetos

--- script.js
+++ script.js
@@ -34,6 +34,25 @@ const weapons = [
     power: 100
   }
 ];
+
+const monsters = [
+  {
+    name: 'slime',
+    level: 2,
+    health: 15
+  },
+  {
+    name: 'fanged beast',
+    level: 8,
+    health: 60
+  },
+  {
+    name: 'dragon',
+    level: 20,
+    health: 300
+  }
+];
+
 const locations = [
   {
     name: 'town square',
@@ -52,6 +71,12 @@ const locations = [
     "button text": ['Fight slime', 'Fight fanged beast', 'Go to town square'],
     "button functions": [fightSlime, fightBeast, goTown],
     text: 'You enter the cave. You see some monsters.'
+  },
+  {
+    name: 'fight',
+    "button text": ['Attack', 'Dodge', 'Run'],
+    "button functions": [attack, dodge, goTown],
+    text: 'You are fighting a monster.'
   }
 ];
 
@@ -80,10 +105,6 @@ function goStore() {
 
 function goCave() {
   update(locations[2]);
-}
-
-function fightDragon() {
-  console.log('Fighting dragon.');
 }
 
 function buyHealth() {
@@ -124,13 +145,39 @@ function sellWeapon() {
     let currentWeapon = inventory.shift();
     text.innerText = 'You sold a ' + currentWeapon + '.';
     text.innerText += ' In your inventory you have: ' + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
 function fightSlime() {
-
+  fighting = 0;
+  goFight();
 }
 
 function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = 'block';
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsters[fighting].health;
+}
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+}
+
+function dodge() {
 
 }
