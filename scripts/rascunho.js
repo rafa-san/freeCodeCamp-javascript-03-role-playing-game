06ec0ba9b4757bf6b5b86a513365e658e5782fcf
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-08-21 10:08:20 -0300

    Criação dos scripts do game e outro para rascunhos

---
+++ scripts/rascunho.js
@@ -0,0 +1,18 @@
+/* 
+let numbers = ["um", "dois", "três"];
+
+let texto = "";
+for (let x in numbers) {
+  texto += numbers[x] + " ";
+}
+
+console.log(texto);
+*/
+
+
+const cat = {
+  name: "Whiskers",
+  "Number of legs": 4,
+}
+console.log(cat.name);
+console.log(cat["Number of legs"]);
